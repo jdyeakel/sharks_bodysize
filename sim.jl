f9d67e2068b0988df0d3fa83e9d4c10e91759d0e
jdyeakel <jdyeakel@gmail.com>
2018-05-04 20:09:35 -0700

    proto code

--- sim.jl
+++ sim.jl
@@ -13,13 +13,21 @@ agevec = collect(0:1/agestep:1);
 
 a = 1;
 m0 = 1;
-M = 1;
+M = 10000;
 eta = 3/4;
 
-function ts(espilon)
-	ts = ln((1-(m0/M)^(1-eta)/(1-epsilon^(1-eta))))*((M^(1-eta)/(a*(1-eta))));
+function ts(epsilon)
+	ts = log((1-(m0/M)^(1-eta)/(1-epsilon^(1-eta))))*((M^(1-eta)/(a*(1-eta))));
 	return ts
 end
+
+
+tvec = Array{Float64}(length(agevec))
+for i=1:length(agevec)
+	epsilon = agevec[i]
+	tvec[i] = ts(epsilon);
+end
+
 
 tvec = mapslices(ts,agevec);
 
