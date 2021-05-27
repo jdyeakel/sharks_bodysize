12b0a5f28104b612018720503bc3bcc0118c9ccb
jdyeakel <jdyeakel@gmail.com>
2021-05-27 16:18:11 -0700

    smaller run for modern

--- sharksims_modern.jl
+++ sharksims_modern.jl
@@ -7,7 +7,7 @@ end
 
 
 #Size at birth (cm)
-l0 = 100.0;
+l0 = 90.0;
 #Asymptotic size (cm)
 # L = 295; #1500; #295.0;
 # NOTE: L = 477 cm (15.65 ft) -> Mass 350 KG -> max tooth length 40 mm
@@ -22,38 +22,7 @@ L = 300;
 # Mass (kg) from TOTAL LENGTH (Goldman et al. 2006)
 # NOTE: Mass of 350 KG (350000g) results in a tooth length of 40 mm (max emp. length)
 # NOTE: Mass of 350 KG results in L = 477 cm = 15.65 ft
-m0 = (0.00013*l0^2.4)*1000;
-M = (0.00013*L^2.4)*1000;
 
-#Sim params
-n0=1000;
-gen=1;
-
-
-mintemp_j = 17;
-maxtemp_j = 25;
-mintemp_a = 13;
-maxtemp_a = 23;
-
-distvec = 700;
-sigtauvec = collect(0.5:0.5:25); # collect(0.5:0.5:25)
-tauvec = collect(1:1:50); #collect(1:1:50);
-tauits = length(sigtauvec)*length(tauvec);
-
-
-#3 paramters: distance, sigtau, tau
-# distposvec = repeat(collect(1:3),inner = length(sigtauvec)*length(tauvec));
-sigtauposvec = repeat(collect(1:length(sigtauvec)),inner = length(tauvec));
-tauposvec = repeat(collect(1:length(tauvec)),outer=length(sigtauvec));
-paramposvec_pre = [sigtauposvec tauposvec];
-#temperature | distance | sigtauvec | tauposvec
-# paramposvec = [repeat(collect(1:4),inner = size(paramposvec_pre)[1]) repeat(paramposvec_pre,outer=4)];
-
-reps = 25;
-paramvec_pre = repeat(paramposvec_pre,outer = reps);
-paramvec = [repeat(collect(1:reps),inner=size(paramposvec_pre)[1]) paramvec_pre];
-
-its = size(paramvec)[1];
 
 @time @sync @distributed for i=1:its
     
