9538ee532eda62e49afc94f5dc63133728ea62c6
jdyeakel <jdyeakel@gmail.com>
2021-06-01 18:52:46 -0700

    added function

--- src/loadfuncs.jl
+++ src/loadfuncs.jl
@@ -25,7 +25,8 @@ if homedir() == "/home/z840"
     @everywhere include("$(homedir())/sharks_bodysize/src/toothdist_analysis.jl")
     @everywhere include("$(homedir())/sharks_bodysize/src/toothdist_emp_analysis.jl")
     @everywhere include("$(homedir())/sharks_bodysize/src/modality_analysis.jl")
-    
+    @everywhere include("$(homedir())/sharks_bodysize/src/empirical_sim_comparison.jl")
+
 else
     
     @everywhere include("$(homedir())/Dropbox/PostDoc/2018_sharks/src/popgen_migrate_g.jl")
@@ -37,5 +38,6 @@ else
     @everywhere include("$(homedir())/Dropbox/PostDoc/2018_sharks/src/toothdist_analysis.jl")
     @everywhere include("$(homedir())/Dropbox/PostDoc/2018_sharks/src/toothdist_emp_analysis.jl")
     @everywhere include("$(homedir())/Dropbox/PostDoc/2018_sharks/src/modality_analysis.jl")
+    @everywhere include("$(homedir())/Dropbox/PostDoc/2018_sharks/src/empirical_sim_comparison.jl")
 
 end
